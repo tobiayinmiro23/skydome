9b0c9661d89a4cd40745b676ead2994971debd07
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-04-06 19:35:02 +0100

    Update Checkout.js
    9

--- src/Checkout.js
+++ src/Checkout.js
@@ -82,7 +82,7 @@ function handleEmail(){
 
   const handleButton=(e)=>{
     if(validName && validAddress && validEmail && validNumber ){
-          fetch(process.env.SERVER_URL,{
+          fetch('https://skydomee.onrender.com/pay,{
                 method:'post',
                 headers:{
                   "Content-Type":"application/json",
