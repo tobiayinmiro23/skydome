80d77e6dc1505dcc77e9c5034ba8018c39c6c1ae
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-09-20 16:21:58 +0100

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,35 +1,19 @@
 import './Checkout.css'
 import React,{useState} from 'react'
-import { Link} from 'react-router-dom';
-  
-const Checkout = ({handlePaymentLink,total}) => {
+import Payment from './Payment';
+const Checkout = ({total}) => {
   const [name, setname] = useState('')
   const [email, setemail] = useState('')
   const [number, setnumber] = useState('')
   const [address, setaddress] = useState('')
-  const [errormsg, seterrormsg] = useState('')
-  const [error, seterror] = useState(false)
   const [validName,setvalidName]=useState(false)
   const [validEmail,setvalidEmail]=useState(false)
   const [validNumber,setvalidNumber]=useState(false)
   const [validAddress,setvalidAddress]=useState(false)
+  const [validPayment, setvalidPayment] = useState(false)
+  const [animateBtn, setanimateBtn] = useState(false)
+  const [paymentLink, setpaymentLink] = useState('')
 
-  const handleInput=()=>{
-    let textarea=document.querySelectorAll('.Checkout .contact  textarea')[0]
-    let inputs=document.querySelectorAll('.Checkout .contact  input')
-  if(textarea.value.trim() === ''){
-    textarea.classList.add('inputerror')
-  }else{
-    textarea.classList.remove('inputerror')
-  }
-  inputs.forEach(item=>{
-      if(item.value.trim() === ''){
-        item.classList.add('inputerror')
-      }else{
-        item.classList.remove('inputerror')
-      }
-    })
- }
 
  const handleName=()=>{
  let nameInput=document.querySelectorAll('.Checkout .contact  input')[0]
@@ -83,7 +67,10 @@ function handleEmail(){
 }
 
   const handleButton=(e)=>{
-    if(validName && validAddress && validEmail && validNumber ){
+    e.preventDefault()
+    setvalidPayment(false)
+    if(validName && validEmail && validAddress  && validNumber){
+      setanimateBtn(true)
           fetch("https://skydomee.onrender.com/pay",{
                 method:'post',
                 headers:{
@@ -94,16 +81,24 @@ function handleEmail(){
                 })
               }).then(res=>res.json())
               .then(res=>{
-                if (res.status === false) alert(res.message)
-                handlePaymentLink(res?.data?.authorization_url)
+                if (res.status === false){
+                  setvalidPayment(false)
+                  setanimateBtn(false)
+                  alert(res.message)
+                }else{
+                  setpaymentLink(res?.data?.authorization_url)
+                  setvalidPayment(true)
+                }
               })
               .catch(err=>{
+                setanimateBtn(false)
+                setvalidPayment(false)
                 if(err.message === 'Failed to fetch'){
                   alert('an error, occured please try again later')
                 }
               })   
     }else{
-      e.preventDefault()
+    e.preventDefault()
     handleNumber()
     handleName()
     handleEmail()
@@ -112,27 +107,37 @@ function handleEmail(){
   }
   return (
     <div className='Checkout'>
-         <section className="contact">
+         {
+          validPayment ?
+
+          <Payment paymentLink={paymentLink}/>
+          :
+
+          <section className="contact">
             <form action='' >
-                <div className="error">{errormsg}</div>
                 <input type="text" className='inp'  required placeholder="name" value={name}  onInput={(e)=>setname(e.target.value)} onChange={()=>handleName()} name="" id=""/>
                 <input type="email" required placeholder="email" value={email} onInput={(e)=>setemail(e.target.value)} onChange={()=>handleEmail()} name="" id=""/>
                 <input type="number" required placeholder="number" value={number} onInput={(e)=>setnumber(e.target.value)} onChange={()=>handleNumber()} name="" id=""/>
                 <textarea required placeholder="address" value={address} onInput={(e)=>setaddress(e.target.value)} onChange={()=>handleAddress()} ></textarea>
                   <div className="btnContainer">
-                <Link to='/payment'>
                     <button onClick={(e)=>handleButton(e)}>
-                      <h4>Proceed to payment</h4> 
-                      <div className="price">
-                        <h3>&#8358;</h3>
-                        <h3>{total.toLocaleString()}</h3>
-                      </div>
+                        {
+                          animateBtn ?
+                            <div className="round"></div>
+                            :
+                            <div >
+                              <h4>Proceed to payment</h4> 
+                              <div className="price">
+                                <h3>&#8358;</h3>
+                                <h3>{total.toLocaleString()}</h3>
+                              </div>
+                            </div>
+                        }
                   </button>
-                  </Link>
-                    
                   </div>
             </form>
-    </section>
+         </section>
+         }
     </div>
   )
 }
