24e4af531101eb9684d4f50ea392f2b43928b493
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-12-28 12:49:31 +0100

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -1,71 +1,78 @@
+
 import React,{useState} from 'react'
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { Link} from 'react-router-dom';
+import {createUserWithEmailAndPassword } from "firebase/auth";
 import {auth} from './firebase'
 import './Signup.css'
-function Signup({user}) {
+import User from './User';
+
+function Signup() {
   const[password,setpassword]=useState('')
   const[email,setemail]=useState('')
   const [errormsg, seterrormsg] = useState('')
   const [loading, setloading] = useState(false)
-  const [valid, setvalid] = useState(false)
+  const [page, setpage] = useState('signin')
 
-    const SignUp = (e) =>{
-      if(valid===false){
-        e.preventDefault()
-      }
-        seterrormsg('')
-        setloading(true)
-        createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            setvalid(true)
-            setloading(false)
-          })
-          .catch((error) => {
-            setvalid(false)
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            setloading(false)
-            seterrormsg(errorCode)
-          if(errorCode === 'auth/internal-error') seterrormsg('unable to sign up please try again later')
-          if(errorCode === 'auth/weak-password') seterrormsg('password should be atleast 6 characters')
-          });
-      }
+
+  const SignUp = (e) =>{
+      e.preventDefault()
+      seterrormsg('')
+      setloading(true)
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          setloading(false)
+          setpage('user')
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setloading(false)
+          seterrormsg(errorCode)
+        if(errorCode === 'auth/internal-error') seterrormsg('unable to sign up please try again later')
+        if(errorCode === 'auth/weak-password') seterrormsg('password should be atleast 6 characters')
+        });
+    }
 
   return (
-      <div className="Signup">
-        <main className="signin">
-              <h1>Sign Up</h1>
-              <form  action="">
-                <div className="input-f">
-                  <div className="img">
-                    <img
-                      src="/E-commerce pictures/email.png"
-                      alt=""
-                    />
-                  </div>
-                  <input  placeholder="Email"  onChange={(e)=>setemail(e.target.value)} value={email} type="email" />
+    <div>
+        {
+            page==='signin' ?
+                <div className="Signup">
+                    <main className="signin">
+                        <h1>Sign Up</h1>
+                        <form  action="">
+                            <div className="input-f">
+                            <div className="img">
+                                <img
+                                src="/E-commerce pictures/email.png"
+                                alt=""
+                                />
+                            </div>
+                            <input  placeholder="Email"  onChange={(e)=>setemail(e.target.value)} value={email} type="email" />
+                            </div>
+                            <div className="input-f">
+                            <div className="img">
+                                <img
+                                src="/E-commerce pictures/padlock.png"
+                                alt=""
+                                />
+                            </div>
+                            <input placeholder="password"  onChange={(e)=>setpassword(e.target.value)} value={password}  type="password" />
+                            </div>
+                                <p className='error'>{errormsg}</p>
+                                    <button className="button" onClick={(e)=>SignUp(e)}>
+                                    {
+                                        loading ? <div className="round"></div> : <h3>Create account</h3>
+                                    }
+                                    </button> 
+                    </form>
+                    </main>
                 </div>
-                <div className="input-f">
-                  <div className="img">
-                    <img
-                      src="/E-commerce pictures/padlock.png"
-                      alt=""
-                    />
-                  </div>
-                  <input placeholder="password"  onChange={(e)=>setpassword(e.target.value)} value={password}  type="password" />
-                </div>
-                    <p className='error'>{errormsg}</p>
-                    <Link to='/user'>
-                        <button className="button" onClick={(e)=>SignUp(e)}>
-                          {
-                            loading ? <div className="round"></div> : <h3>Create account</h3>
-                          }
-                          </button> 
-                    </Link>
-          </form>
-        </main>
-      </div>
+                :
+              <User/>
+
+        }
+    </div>
+
 
         )
 }
