2b0170a0fd16a5237b26264f9d481ac2db148abf
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-12-28 12:39:26 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,75 +1,82 @@
-import React,{useState} from 'react'
-import { getAuth, signInWithEmailAndPassword ,sendPasswordResetEmail} from "firebase/auth";
-// import {auth} from './firebase'
-import { Link} from 'react-router-dom';
 
-function Login({user}) {
+import React,{useState} from 'react'
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { Link} from 'react-router-dom';
+import User from './User';
+
+
+function Login() {
   const[password,setpassword]=useState('')
   const[email,setemail]=useState('')
   const [errormsg, seterrormsg] = useState('')
   const [loading, setloading] = useState(false)
-  const [valid, setvalid] = useState(false)
+  const [page, setpage] = useState('login')
+
   const auth = getAuth();
 
-    const SignIn = (e)=>{
-      if(valid===false){
-        e.preventDefault()
-      }
-      seterrormsg('')
-      setloading(true)
-      signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          const user = userCredential.user;
-          setvalid(true)
-          setloading(false)
-        })
+  const SignIn = (e)=>{
+      e.preventDefault()
+    seterrormsg('')
+    setloading(true)
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setloading(false)
+        setpage('user')
+      })
 
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          setvalid(false)
-          setloading(false)
-          seterrormsg(errorCode)
-          if(errorCode === 'auth/internal-error') seterrormsg('unable to sign in please try again later')
-        });
-           
-            
-    }
+      .catch((error) => {
+        e.preventDefault()
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setloading(false)
+        seterrormsg(errorCode)
+        if(errorCode === 'auth/internal-error') seterrormsg('unable to sign in please try again later')
+      });
+         
+          
+  }
      return (
-        <div className="Signup">
-      <main className="signin">
-            <h1>sign in</h1>
-            <form  action="">
-              <div className="input-f">
-                <div className="img">
-                  <img
-                    src="/E-commerce pictures/email.png"
-                    alt=""
-                  />
-                </div>
-                <input  placeholder="Email"  onChange={(e)=>setemail(e.target.value)} value={email} type="email" />
+      <div>
+        {
+            page==='login' ?
+            <div className="Signup">
+            <main className="signin">
+                  <h1>sign in</h1>
+                  <form  action="">
+                    <div className="input-f">
+                      <div className="img">
+                        <img
+                          src="/E-commerce pictures/email.png"
+                          alt=""
+                        />
+                      </div>
+                      <input  placeholder="Email"  onChange={(e)=>setemail(e.target.value)} value={email} type="email" />
+                    </div>
+                    <div className="input-f">
+                      <div className="img">
+                        <img
+                          src="/E-commerce pictures/padlock.png"
+                          alt=""
+                        />
+                      </div>
+                      <input placeholder="password"  onChange={(e)=>setpassword(e.target.value)} value={password}  type="password" />
+                    </div>
+                    <p className="forgotPassswordLink"><Link to='/forgotpassword' > forgot password ?</Link></p>
+                        <p className='error'>{errormsg}</p>
+                            <button className="button" style={{marginTop:"0.5rem"}} onClick={SignIn}>
+                              {
+                                loading ? <div className="round"></div> : <h3>Sign In</h3>
+                              }
+                              </button> 
+              </form>
+            </main>
               </div>
-              <div className="input-f">
-                <div className="img">
-                  <img
-                    src="/E-commerce pictures/padlock.png"
-                    alt=""
-                  />
-                </div>
-                <input placeholder="password"  onChange={(e)=>setpassword(e.target.value)} value={password}  type="password" />
-              </div>
-              <p className="forgotPassswordLink"><Link to='/forgotpassword' > forgot password ?</Link></p>
-                  <p className='error'>{errormsg}</p>
-                  <Link to='/user'>
-                      <button className="button" style={{marginTop:"0.5rem"}} onClick={SignIn}>
-                        {
-                          loading ? <div className="round"></div> : <h3>Sign In</h3>
-                        }
-                        </button> 
-                  </Link>
-        </form>
-      </main>
-        </div>
+              :
+              <User/>
+
+        }
+      </div>
   )
 }
 
